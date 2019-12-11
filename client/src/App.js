d39b476115d244ad5e8a0a303b7eb4db0814b651
Luke Prescott <luke.prescott@presport.us>
2019-12-11 10:40:34 -0500

    connected apollo client to react

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { gql } from "apollo-boost";
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
 
 const GET_TODOS = gql`
   query {
@@ -13,24 +16,15 @@ const GET_TODOS = gql`
   }
 `;
 
+const client = new ApolloClient();
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+      </div>
+    </ApolloProvider>
   );
 }
 
