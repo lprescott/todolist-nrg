63041cdd6d5bbbab45f26376150905eb74fff0c4
Luke Prescott <luke.prescott@presport.us>
2019-12-11 11:35:49 -0500

    implementation of addTodo, need to update cache

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,89 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { gql } from "apollo-boost";
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
-import { useQuery } from '@apollo/react-hooks';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { useQuery } from "@apollo/react-hooks";
+import { useMutation } from "@apollo/react-hooks";
 
 const client = new ApolloClient();
 
 function Todos() {
-  const { loading, error, data } = useQuery(gql`
-    {
-      todos {
-        id
-        text
-        completed
-      }
-    }
-  `);
+    const { loading, error, data } = useQuery(gql`
+        {
+            todos {
+                id
+                text
+                completed
+            }
+        }
+    `);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
 
-  return data.todos.map(({ id, text, completed }) => (
-    <div class="todo" key={id}>
-      <p>
-        {(completed) ? 'done:' : 'todo'} {text}
-      </p>
-    </div>
-  ));
+    return data.todos.map(({ id, text, completed }) => (
+        <div class="todo" id={"todo-" + id}>
+            <p>
+                {completed ? "done:" : "todo"} {text}
+            </p>
+        </div>
+    ));
 }
 
 function AddTodo() {
-  return (
-    <div class="addTodo">
-      <input type="text" placeholder="Add a new todo..."/>
-      <button type="submit">Submit</button>
-    </div>
-  )
+    const ADD_TODO = gql`
+        mutation addTodo($text: String!) {
+            addTodo(text: $text) {
+                code
+                success
+                message
+                todo {
+                    id
+                    text
+                    completed
+                }
+            }
+        }
+    `;
+
+    let input;
+    const [addTodo, { data }] = useMutation(ADD_TODO);
+
+    return (
+        <form
+            class="addTodo"
+            onSubmit={e => {
+                e.preventDefault();
+                addTodo({ variables: { text: input.value } });
+                input.value = '';
+            }}
+        >
+            <input
+                type="text"
+                placeholder="Add a new todo..."
+                ref={node => {
+                    input = node;
+                }}
+            />
+            <button type="submit">Submit</button>
+        </form>
+    );
 }
 
 const App = () => {
-  return (
-    <ApolloProvider client={client}>
-      <h2>My first Apollo app 
-        <span aria-label="rocket-emoji" role="img">🚀</span>
-      </h2>
-      <AddTodo />
-      <Todos />
-    </ApolloProvider>
-  );
-}
+    return (
+        <ApolloProvider client={client}>
+            <h2>
+                My first Apollo app
+                <span aria-label="rocket-emoji" role="img">
+                    🚀
+                </span>
+            </h2>
+            <AddTodo />
+            <Todos />
+        </ApolloProvider>
+    );
+};
 
 export default App;
