060e14eed9ebe5fc282900208b67e1f8c38a1103
Luke Prescott <luke.prescott@presport.us>
2019-12-11 09:31:31 -0500

    Fixed mutators to todolist model, addTodo deleteTodo...

--- client/src/App.js
+++ client/src/App.js
@@ -1,119 +1,26 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Todos from './components/Todos';
-import Header from './components/layout/Header';
-import AddTodo from './components/AddTodo';
-import About from './components/pages/About';
-import uuid from 'uuid';
-import ApolloClient from 'apollo-boost';
-import { gql } from 'apollo-boost';
-
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-const GET_TODOS = gql`
-    {
-        todos {
-            id
-            text
-            completed
-        }
-    }
-`;
-
-class App extends Component {
-    state = {
-        todos: [],
-    };
-
-    componentDidMount() {
-        // TODO: get todos from server here
-        const client = new ApolloClient();
-
-        client
-            .query({
-                query: GET_TODOS,
-            })
-            .then(result => this.setState({ todos: result.data.todos }));
-    }
-
-    // Toggle Completed
-    toggleComplete = id => {
-        // TODO: toggleComplete to graphQL
-
-        // toggleComplete to UI
-        this.setState({
-            todos: this.state.todos.map(todo => {
-                if (todo.id === id) {
-                    todo.completed = !todo.completed;
-                }
-                return todo;
-            }),
-        });
-    };
-
-    deleteTodo = id => {
-        // TODO: deleteTodo to graphQL
-
-        // deleteTodo to UI
-        this.setState({
-            todos: this.state.todos.filter(todo => todo.id !== id),
-        });
-    };
-
-    updateTodo = (id, text) => {
-        // TODO: updateTodo to graphQL
-
-        // updateTodo to UI
-        this.setState({
-            todos: this.state.todos.map(todo => {
-                if (todo.id === id) {
-                    todo.text = text;
-                }
-                return todo;
-            }),
-        });
-    };
-
-    addTodo = text => {
-        // TODO: addTodo to graphQL
-
-        // addTodo to UI
-        const newTodo = {
-            id: uuid.v4(),
-            text,
-            completed: false,
-        };
-        // usage of the spread operator to copy initial state
-        this.setState({ todos: [...this.state.todos, newTodo] });
-    };
-
-    render() {
-        return (
-            <Router>
-                <div className="App">
-                    <div className="container">
-                        <Header />
-                        <Route
-                            exact
-                            path="/"
-                            render={props => (
-                                <React.Fragment>
-                                    <AddTodo addTodo={this.addTodo} />
-                                    <Todos
-                                        todos={this.state.todos}
-                                        toggleComplete={this.toggleComplete}
-                                        deleteTodo={this.deleteTodo}
-                                        updateTodo={this.updateTodo}
-                                    />
-                                </React.Fragment>
-                            )}
-                        />
-                        <Route path="/about" component={About} />
-                    </div>
-                </div>
-            </Router>
-        );
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
---
+++ server/src/index.js
@@ -0,0 +1,162 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  interface MutationResponse {
+    code: String!
+    success: Boolean!
+    message: String!
+  }
+
+  type TodoMutationResponse implements MutationResponse {
+    code: String!
+    success: Boolean!
+    message: String!
+    todo: Todo
+  }
+
+  # todo model
+  type Todo {
+    id: ID
+    text: String
+    completed: Boolean
+  }
+
+  # queries: returnables
+  type Query {
+    todos: [Todo]
+  }
+
+  type Mutation {
+    addTodo(text: String): TodoMutationResponse
+    deleteTodo(id: ID): TodoMutationResponse
+    updateTodo(id: ID, text: String): TodoMutationResponse
+    toggleTodo(id: ID): TodoMutationResponse
+  }
+`;
+
+// the todos list
+var todos = [
+  {
+    id: 1,
+    text: "Take out the trash!",
+    completed: false
+  },
+  {
+    id: 2,
+    text: "Learn the energy stack.",
+    completed: true
+  },
+  {
+    id: 3,
+    text: "Full 8 hours.",
+    completed: false
+  },
+  {
+    id: 4,
+    text: "Clean room.",
+    completed: false
+  },
+  {
+    id: 5,
+    text: "Practice guitar.",
+    completed: true
+  }
+];
+
+// how to fetch the data (hard-coded)
+let todoCount = todos.length;
+const resolvers = {
+  Query: {
+    todos: () => todos
+  },
+  Mutation: {
+    addTodo: (parent, args) => {
+      const todo = {
+        id: ++todoCount,
+        text: args.text,
+        completed: false
+      };
+      todos.push(todo);
+      return {
+        code: "200",
+        success: true,
+        message: "Successfully added todo.",
+        todo
+      };
+    },
+    deleteTodo: (parent, args) => {
+      let flag = false;
+      todos = todos.filter(todo => {
+        if (todo.id != args.id) return todo;
+        else flag = true;
+      });
+      if (flag == true) {
+        return {
+          code: "200",
+          success: true,
+          message: "Successfully deleted todo."
+        };
+      } else {
+        return {
+          code: "404",
+          success: false,
+          message: "Could not find todo."
+        };
+      }
+    },
+    updateTodo: (parent, args) => {
+      for (var i = 0; i < todos.length; i++) {
+        if (todos[i].id == args.id) {
+          const todo = todos[i];
+          if (todos[i].text == args.text) {
+            return {
+              code: "304",
+              success: false,
+              message: "No change.",
+              todo
+            };
+          } else {
+            todos[i].text = args.text;
+            return {
+              code: "200",
+              success: true,
+              message: "Successfully updated todo.",
+              todo
+            };
+          }
+        }
+      }
+      return {
+        code: "404",
+        success: false,
+        message: "Could not find todo."
+      };
+    },
+    toggleTodo: (parent, args) => {
+      for (var i = 0; i < todos.length; i++) {
+        if (todos[i].id == args.id) {
+          const todo = todos[i];
+          todos[i].completed = !todos[i].completed;
+          return {
+            code: "200",
+            success: true,
+            message: "Successfully updated todo.",
+            todo
+          };
+        }
+      }
+      return {
+        code: "404",
+        success: false,
+        message: "Could not find todo."
+      };
+    }
+  }
+};
+
+// create new server with the above typeDefs and resolvers
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}graphql`);
+});
