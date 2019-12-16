659b673ee5c81484a16025ac0905d9b45f3d82f0
Luke Prescott <luke.prescott@presport.us>
2019-12-16 16:17:01 -0500

    Cleaned up redundant styling

--- client/src/TodoList/TodoList.js
+++ client/src/TodoList/TodoList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./TodoList.css";
+import "../index.css";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { useQuery } from "@apollo/react-hooks";
